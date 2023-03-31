82c49438a20a7c9bfdec07785962b7a7276f52e2
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2023-03-31 21:34:59 +0000

    Styl using styled-compenents

--- pages/Modal.jsx
+++ pages/Modal.jsx
@@ -1,4 +1,51 @@
-import React, { useState } from "react";
+import React from "react";
+import styled from "styled-components";
+
+const Popup = styled.div`
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background: rgba(0, 0, 0, 0.7);
+`;
+
+const ModalOverlay = styled.div`
+  background: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  top: 10rem;
+  left: 30rem;
+  height: 12rem;
+  width: 15rem;
+  border-radius: 7%;
+`;
+
+const ModalContent = styled.div`
+  padding: 0 1rem;
+`;
+const FlexButton = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const CancelButton = styled.button`
+  background: white;
+  border: 1px solid black;
+  border-radius: 10%;
+  padding: 0.4rem;
+`;
+
+const SecondButton = styled.button`
+  background: blue;
+  color: white;
+  border-radius: 10%;
+  padding: 0.4rem;
+  outline: none;
+`;
 
 const Modal = ({ isOpen, onClose, children }) => {
   if (!isOpen) {
@@ -6,15 +53,17 @@ const Modal = ({ isOpen, onClose, children }) => {
   }
 
   return (
-    <div className="modal">
-      <div className="modal-overlay"></div>
-      <div className="modal-content">
-        <button className="modal-close" onClick={onClose}>
-          X
-        </button>
-        {children}
-      </div>
-    </div>
+    <Popup>
+      <ModalOverlay>
+        <ModalContent>
+          {children}
+          <FlexButton>
+            <CancelButton onClick={onClose}>Cancel</CancelButton>
+            <SecondButton>Open an email</SecondButton>
+          </FlexButton>
+        </ModalContent>
+      </ModalOverlay>
+    </Popup>
   );
 };
 
